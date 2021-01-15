148bce29dcfa22d4f705ce66332ea1a8b04e3746
Elias <elias@Eliass-MacBook-Pro.local>
2021-01-14 23:05:20 -0800

    Completed Lab 2

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     initializePage();
-});
-
-function initializePage() {
+  });
+  
+  function initializePage() {
     $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
-}
+      $('.jumbotron h1').text("Javascript is connected");
+      });
+    $("a.thumbnail").click(projectClick);
+  }
+  
+  function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+  }
